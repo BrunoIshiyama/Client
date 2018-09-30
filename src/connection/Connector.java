dcf0fc0bcfc9f5d8da11772ea010b68ab4a34f75
Bruno Yuji Ishiyama <bruno.yuji.ishiyama@ibm.com>
2018-09-30 13:04:33 -0300

    minor changes to the format of sending messages

--- src/connection/Connector.java
+++ src/connection/Connector.java
@@ -1,33 +1,67 @@
 package connection;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Connector {
 	private static Connector instance;
 	private static Socket clientSocket;
 	public static final int PORT = 9797;
 	public String host;
+
 	private Connector(String host) throws UnknownHostException, IOException {
 		this.host = host;
 		clientSocket = new Socket(host, PORT);
 	}
+
 	public static Connector getInstance(String host) throws UnknownHostException, IOException {
-		if(instance == null) return instance = new Connector(host);
+		if (instance == null)
+			return instance = new Connector(host);
 		return instance;
 	}
+
 	public void send(String msg) throws IOException {
 		OutputStream info = clientSocket.getOutputStream();
 		info.write(msg.getBytes());
 		info.flush();
 	}
+
 	public static void main(String[] args) {
 		try {
 			Connector c = Connector.getInstance("172.115.13.95");
-			c.send("Hello World");
-			c.clientSocket.close();
+			InputStream is = clientSocket.getInputStream();
+			Scanner sc = new Scanner(System.in);
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							c.send(sc.nextLine().trim());
+							System.out.println("next");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+
+				}
+			}).start();
+			while (clientSocket.isConnected()) {
+
+				byte[] bytes = new byte[is.available()];
+				is.read(bytes);
+				String d = new String(bytes);
+				if (d.equals("end"))
+					break;
+				System.out.print(d);
+
+			}
+			Connector.clientSocket.close();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
