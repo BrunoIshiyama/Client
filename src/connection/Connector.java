1dcc73b374dcfcc45c804ad27f50ae5ab25665a3
R1pp3r <r1pp3r@localhost.localdomain>
2018-10-06 17:23:08 -0300

    client with encryption

--- src/connection/Connector.java
+++ src/connection/Connector.java
@@ -7,13 +7,23 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+import decryptor.Decryptor;
+import encryptor.Encryptor;
+
 public class Connector {
 	private static Connector instance;
 	private static Socket clientSocket;
 	public static final int PORT = 9797;
 	public String host;
 	public boolean readLock = false;
-	public boolean keepAlive = true;
+	public boolean Alive = true;
+	static long primeP = 13;
+	static long primeQ = 31;
+	static long keyE = 11;
+	static long serverKey = 19*23;
+	static long serverKeyE = 7;
+	public static Decryptor dec = new Decryptor(primeP, primeQ, keyE);
+	public static Encryptor enc = new Encryptor(serverKey, serverKeyE);
 	private Connector(String host) throws UnknownHostException, IOException {
 		this.host = host;
 		clientSocket = new Socket(host, PORT);
@@ -27,6 +37,7 @@ public class Connector {
 
 	public void send(String msg) throws IOException {
 		OutputStream info = clientSocket.getOutputStream();
+		msg = enc.encrypt(msg);
 		info.write(msg.getBytes());
 		info.flush();
 	}
@@ -35,8 +46,8 @@ public class Connector {
 		try {
 			// 172.115.13.95
 			System.out.println("Connecting to host...");
-			Connector c = Connector.getInstance("192.168.0.102");
-			System.out.println("Connected to "+Connector.clientSocket.getInetAddress());
+			Connector c = Connector.getInstance("192.168.0.110");
+			System.out.println("Connected to " + Connector.clientSocket.getInetAddress());
 			InputStream is = clientSocket.getInputStream();
 			Scanner sc = new Scanner(System.in);
 			Thread job = new Thread(new Runnable() {
@@ -44,11 +55,11 @@ public class Connector {
 				@Override
 				public void run() {
 					System.out.println("Type a command:");
-					LOOP:
-					while (true) {
+					LOOP: while (true) {
 						try {
 							String s = "";
-							if (!(s=sc.nextLine().trim()).equals("")&&!s.isEmpty()&&s.length()>0) {
+							System.out.print("> ");
+							if (!(s = sc.nextLine().trim()).equals("") && !s.isEmpty() && s.length() > 0) {
 								c.send(s);
 								c.readLock = true;
 								try {
@@ -58,18 +69,20 @@ public class Connector {
 									e.printStackTrace();
 								}
 								while (c.readLock) {
-									byte[] bytes = new byte[is.available()];
-									is.read(bytes);
-									String d = new String(bytes);
-									if (d.equals("end")) {
+									String d = dec.decrypt(is);
+									try {
+										Thread.sleep(1000);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									if (d.trim().equals("end")) {
 										System.out.println("Connection closed");
-										c.keepAlive= false;
+										c.Alive = false;
 										c.readLock = false;
-										sc.close();
-										Connector.clientSocket.close();
 										break LOOP;
-									}else {
-										System.out.println("SERVER: "+d);
+									} else {
+										System.out.println(d);
 										System.out.println();
 										c.readLock = false;
 									}
@@ -77,18 +90,26 @@ public class Connector {
 							}
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
-							e.printStackTrace();
+							c.Alive = false;
+							System.out.println("Connection with server lost");
+							break LOOP;
 						}
 					}
 
 				}
 			});
 			job.start();
-			while (c.keepAlive) {}
-			job.interrupt();
+
+			try {
+				job.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			sc.close();
 			Connector.clientSocket.close();
 			System.exit(0);
-			
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
